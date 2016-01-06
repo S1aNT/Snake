8fc78bf996675492b029df923ebdaa09f702b9e6
slant <p.slant@gmail.com>
2016-01-07 00:56:18 +0300

    Example RenderView:Paint  Blue square  Red line  Green Circle

--- app/src/main/java/com/zanyxdev/snake/MainFullscreenActivity.java
+++ app/src/main/java/com/zanyxdev/snake/MainFullscreenActivity.java
@@ -12,6 +12,6 @@ public class MainFullscreenActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_fullscreen);
+        setContentView(new RenderView(this));
     }
 }
