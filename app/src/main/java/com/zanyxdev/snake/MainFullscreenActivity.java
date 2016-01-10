d22ee787f82260ffcb2d225558cb95bbd6f88bf1
slant <p.slant@gmail.com>
2016-01-11 01:53:28 +0300

    swipe detector relise

--- app/src/main/java/com/zanyxdev/snake/MainFullscreenActivity.java
+++ app/src/main/java/com/zanyxdev/snake/MainFullscreenActivity.java
@@ -1,7 +1,15 @@
 package com.zanyxdev.snake;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import com.zanyxdev.snake.framework.IOnSwipeGestureEventListener;
+import com.zanyxdev.snake.framework.SwipeDirection;
+import com.zanyxdev.snake.framework.onGestureListener;
 
 
 /**
@@ -9,9 +17,53 @@ import android.os.Bundle;
  * status bar and navigation/system bar) with user interaction.
  */
 public class MainFullscreenActivity extends Activity {
+    private GestureDetector gestureDetector;
+    private Context mcontext;
+
+    private onGestureListener listener;
+//    private ScreenSize screenSize;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new RenderView(this));
+        setContentView(R.layout.activity_main_fullscreen);
+        mcontext = this;
+        //      screenSize = new ScreenSize(mcontext);
+
+        // Detect touched area
+        listener = new onGestureListener();
+        listener.setSwipeGestureEventListener(new IOnSwipeGestureEventListener() {
+            @Override
+            public void onEvent(SwipeDirection direction) {
+                String str = "";
+
+                switch (direction) {
+
+                    case RIGHT:
+                        str = "Swipe Right";
+                        break;
+                    case LEFT:
+                        str = "Swipe Left";
+                        break;
+                    case DOWN:
+                        str = "Swipe Down";
+                        break;
+                    case UP:
+                        str = "Swipe Up";
+                        break;
+                }
+                Toast.makeText(mcontext, str, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        gestureDetector = new GestureDetector(this, listener);
+
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+    }
+
+
 }
